2defe2e9b4e5e7d8d489bbd9d528148e5afe79c0
Noah-Jaffe <noah0m0jaffe@gmail.com>
2025-03-07 16:23:20 -0500

    test stuff

--- sun_3_2_2025.py
+++ sun_3_2_2025.py
@@ -4,8 +4,7 @@ import os
 from math import log10
 from tkinter import END, HORIZONTAL, LEFT, SOLID, Button, Label, Listbox, StringVar, Toplevel, filedialog, messagebox, ttk, Tk
 from datetime import datetime
-import traceback
-import pylangacq
+from gooey import Gooey, GooeyParser
 
 TEMP_INP_FILES_DIR = "./.tmp_inp"
 TEMP_OUT_FILES_DIR = "./.tmp_out"
@@ -18,22 +17,27 @@ MODELS_DIRECTORY="./models" # where to save the models to
 EXPECTED_RAW_FILE_TYPE='mp4' # placeholder, this might not be needed
 BEST_FILE_TYPE_FOR_TRANSCRIPTION='wav' # placeholder, this might not be needed
 
-# transcribe
-whisper = ba.WhisperEngine(model=None, lang="eng")
-diarization = ba.NemoSpeakerEngine(num_speakers=2)
-disfluency = ba.DisfluencyReplacementEngine()
-retrace = ba.NgramRetraceEngine()
-# morphotag
-morphosyntax = ba.StanzaEngine()
-# align
-utr = ba.WhisperUTREngine()
-fa = ba.Wave2VecFAEngine()
+@Gooey()
+def main():
+    # transcribe
+    whisper = ba.WhisperEngine(model=None, lang="eng")
+    diarization = ba.NemoSpeakerEngine(num_speakers=2)
+    disfluency = ba.DisfluencyReplacementEngine()
+    retrace = ba.NgramRetraceEngine()
+    # morphotag
+    morphosyntax = ba.StanzaEngine()
+    # align
+    utr = ba.WhisperUTREngine()
+    fa = ba.Wave2VecFAEngine()
 
 
-# create a pipeline
-nlp = ba.BatchalignPipeline(whisper, diarization, disfluency, retrace, morphosyntax, utr, fa)
-doc = ba.Document.new(media_path=".tmp_test/in/clip2.mp3", lang="eng")
-doc = nlp(doc)
-print(1)
-chat = ba.CHATFile(doc=doc)
-chat.write(".tmp_test/out/clip.cha")
+    # create a pipeline
+    nlp = ba.BatchalignPipeline(whisper, diarization, disfluency, retrace, morphosyntax, utr, fa)
+    doc = ba.Document.new(media_path=".tmp_test/in/clip2.mp3", lang="eng")
+    doc = nlp(doc)
+    print(1)
+    chat = ba.CHATFile(doc=doc)
+    chat.write(".tmp_test/out/clip.cha")
+
+if __name__ == "__main__":
+    main()
