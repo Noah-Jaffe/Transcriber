f8332e46f9a71a8b9aa3c3836088aa7e417a3ca8
Noah-Jaffe <noah0m0jaffe@gmail.com>
2025-07-02 18:32:10 -0400

    cleanup and error handling

--- transcribe_proc.py
+++ transcribe_proc.py
@@ -6,8 +6,7 @@ import json
 from types import FunctionType
 from huggingface_hub.hf_api import repo_exists as is_valid_model_id
 import pycountry
-from CustomAiEngine import CustomAiEngine
-import traceback
+# from CustomAiEngine import CustomAiEngine
 
 
 def transcribe_file(input_file, model_name=None, num_speakers=2, lang="eng"):
@@ -46,7 +45,7 @@ def transcribe_file(input_file, model_name=None, num_speakers=2, lang="eng"):
         retrace,  # uncertain how this benifits us
         # morphosyntax,
         utr,
-        # fa
+        fa
     ] if action]
     
     n = 0
@@ -66,31 +65,19 @@ def transcribe_file(input_file, model_name=None, num_speakers=2, lang="eng"):
             with open(output_file,'a',encoding='utf-8') as f:
                 f.write(f"@DEBUG Completed step {idx}/{len(pipeline_activity)} - {(type(activity).__name__).replace('Engine','')}\n")
         except Exception as e:
-            print(traceback.format_exc(e))
-            print(f"{output_file} made it to step: {idx-1}/{len(pipeline_activity)}")
             with open(output_file,'a',encoding='utf-8') as f:
                 f.write(f"@DEBUG error during step {idx}/{len(pipeline_activity)} - {(type(activity).__name__).replace('Engine','')}\n")
-        
-        
-    # create a pipeline
-    # nlp = ba.BatchalignPipeline(*pipeline_activity)
-    # doc = ba.Document.new(media_path=input_file, lang=lang)
-    # doc = nlp(doc)
-    # chat = ba.CHATFile(doc=doc)
-    # n = 0
-    # output_file = f"{input_file}{'_'+str(n) if n > 0 else ''}.cha"
-    # while 1:
-    #     output_file = f"{input_file}{'_'+str(n) if n > 0 else ''}.cha"
-    #     if not os.path.exists(output_file):
-    #         break
-    #     n += 1
-    # chat.write(output_file, write_wor=False)
+            print(e)
+            print(f"{output_file} made it to step: {idx-1}/{len(pipeline_activity)}")
+            
+    
     print(f"Wrote to {output_file}", flush=True)
+    # uncomment this next block if you want the output file to automatically open
+    # this process is blocking so we dont do it for now so that we can run through the rest of the files given by the UI component
     # try:
     #     os.startfile(output_file)
     # except:
     #     pass
-    # this is process blocking so we dont do it for now
     # return spawn_popup_activity(title="COMPLETED!",message=f"Completed transcription of\n{input_file}\nOutput file can be found here:\n{output_file}\nOpen file now?", yes=lambda: os.startfile(output_file))
 
 def spawn_popup_activity(title, message, yes=None, no=None):
@@ -101,8 +88,6 @@ def spawn_popup_activity(title, message, yes=None, no=None):
         return no()
 
 if __name__ == "__main__":
-    sys.argv.append('{"input_file":"D:\\\\Parent-Child Interactions\\\\Parent-Child Interactions\\\\P002_PCI.mp3","num_speakers":3,"lang":"","model_name":"openai/whisper-base.en"}')
-    print("Attempting to transcribe for:", sys.argv[1:], flush=True)
     print(sys.argv, flush=True)
     for data in sys.argv[1:]:
         try:
@@ -110,5 +95,6 @@ if __name__ == "__main__":
         except:
             print(f"Failed to parse input data: {data}")
             continue
+        print("Attempting to transcribe for:", args.get('input_file',args), flush=True)
         transcribe_file(**args)
-    print("Attempt completed for:", sys.argv[1:], flush=True)
+        print("Attempt completed for:", sys.argv[1:], flush=True)
